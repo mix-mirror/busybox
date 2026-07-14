mix-mirror/busybox

commit 56bbbfae7df48f778362d1848d2a4afd2c293d76

cp: implement -n
function                                             old     new   delta
.rodata                                           103681  103722     +41
packed_usage                                       33698   33717     +19
copy_file                                           1678    1696     +18
cp_main                                              500     492      -8
------------------------------------------------------------------------------
(add/remove: 0/0 grow/shrink: 3/1 up/down: 78/-8)              Total: 70 bytes

Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

coreutils/cp.c
@@ -84,8 +84,8 @@
 //	(SELinux) set SELinux security context of copy to CONTEXT
 
 //usage:#define cp_trivial_usage
-//usage:       "[-arPLHpfilsTu] SOURCE DEST\n"
-//usage:       "or: cp [-arPLHpfilsu] SOURCE... { -t DIRECTORY | DIRECTORY }"
+//usage:       "[-arPLHpfinlsTu] SOURCE DEST\n"
+//usage:       "or: cp [-arPLHpfinlsu] SOURCE... { -t DIRECTORY | DIRECTORY }"
 //usage:#define cp_full_usage "\n\n"
 //usage:       "Copy SOURCEs to DEST\n"
 //usage:     "\n	-a	Same as -dpR"
@@ -99,6 +99,7 @@
 //usage:     "\n	-p	Preserve file attributes if possible"
 //usage:     "\n	-f	Overwrite"
 //usage:     "\n	-i	Prompt before overwrite"
+//usage:     "\n	-n	Don't overwrite"
 //usage:     "\n	-l,-s	Create (sym)links"
 //usage:     "\n	-T	Refuse to copy if DEST is a directory"
 //usage:     "\n	-t DIR	Copy all SOURCEs into DIR"
@@ -122,9 +123,9 @@ int cp_main(int argc, char **argv)
 	int status;
 	enum {
 #if ENABLE_FEATURE_CP_LONG_OPTIONS
-		/*OPT_rmdest  = FILEUTILS_RMDEST = 1 << FILEUTILS_CP_OPTNUM */
-		OPT_parents = 1 << (FILEUTILS_CP_OPTNUM+1),
-		OPT_reflink = 1 << (FILEUTILS_CP_OPTNUM+2),
+		/*OPT_rmdest  = FILEUTILS_RMDEST = 1 << FILEUTILS_CP_OPTBITS */
+		OPT_parents = 1 << (FILEUTILS_CP_OPTBITS+1),
+		OPT_reflink = 1 << (FILEUTILS_CP_OPTBITS+2),
 #endif
 	};
 #if ENABLE_FEATURE_CP_LONG_OPTIONS
@@ -134,23 +135,25 @@ int cp_main(int argc, char **argv)
 	flags = getopt32long(argv, "^"
 		FILEUTILS_CP_OPTSTR
 		"\0"
-		// Need at least two arguments
+		// Need at least one argument. (Usually two+, but -t DIR can have only one)
 		// Soft- and hardlinking doesn't mix
 		// -P and -d are the same (-P is POSIX, -d is GNU)
 		// -r and -R are the same
 		// -R (and therefore -r) turns on -d (coreutils does this)
 		// -a = -pdR
-	/* At least one argument. (Usually two+, but -t DIR can have only one) */
-		"-1:l--s:s--l:Pd:rRd:Rd:apdR",
+		// -i overrides -n and vice versa (last wins)
+		"-1:l--s:s--l:Pd:rRd:Rd:apdR:i-n:n-i",
 		"archive\0"        No_argument "a"
 		"force\0"          No_argument "f"
 		"interactive\0"    No_argument "i"
+		"no-clobber\0"     No_argument "n"
 		"link\0"           No_argument "l"
 		"dereference\0"    No_argument "L"
 		"no-dereference\0" No_argument "P"
 		"recursive\0"      No_argument "R"
 		"symbolic-link\0"  No_argument "s"
 		"no-target-directory\0" No_argument "T"
+		"target-directory\0" Required_argument "t"
 		"verbose\0"        No_argument "v"
 		"update\0"         No_argument "u"
 		"remove-destination\0" No_argument "\xff"

include/libbb.h
@@ -450,28 +450,29 @@ enum {	/* cp.c, mv.c, install.c depend on these values. CAREFUL when changing th
 	FILEUTILS_RECUR           = 1 << 2, /* -R */
 	FILEUTILS_FORCE           = 1 << 3, /* -f */
 	FILEUTILS_INTERACTIVE     = 1 << 4, /* -i */
-	FILEUTILS_MAKE_HARDLINK   = 1 << 5, /* -l */
-	FILEUTILS_MAKE_SOFTLINK   = 1 << 6, /* -s */
-	FILEUTILS_DEREF_SOFTLINK  = 1 << 7, /* -L */
-	FILEUTILS_DEREFERENCE_L0  = 1 << 8, /* -H */
+	FILEUTILS_NO_OVERWRITE    = 1 << 5, /* -n */
+	FILEUTILS_MAKE_HARDLINK   = 1 << 6, /* -l */
+	FILEUTILS_MAKE_SOFTLINK   = 1 << 7, /* -s */
+	FILEUTILS_DEREF_SOFTLINK  = 1 << 8, /* -L */
+	FILEUTILS_DEREFERENCE_L0  = 1 << 9, /* -H */
 	/* -a = -pdR (mapped in cp.c) */
 	/* -r = -dR  (mapped in cp.c) */
 	/* -P = -d   (mapped in cp.c) */
-	FILEUTILS_VERBOSE         = (1 << 12) * ENABLE_FEATURE_VERBOSE,	/* -v */
-	FILEUTILS_UPDATE          = 1 << 13, /* -u */
-	FILEUTILS_NO_TARGET_DIR	  = 1 << 14, /* -T */
-	FILEUTILS_TARGET_DIR	  = 1 << 15, /* -t DIR */
+	FILEUTILS_VERBOSE         = (1 << 13) * ENABLE_FEATURE_VERBOSE,	/* -v */
+	FILEUTILS_UPDATE          = 1 << 14, /* -u */
+	FILEUTILS_NO_TARGET_DIR	  = 1 << 15, /* -T */
+	FILEUTILS_TARGET_DIR	  = 1 << 16, /* -t DIR */
 #if ENABLE_SELINUX
-	FILEUTILS_PRESERVE_SECURITY_CONTEXT = 1 << 16, /* -c */
+	FILEUTILS_PRESERVE_SECURITY_CONTEXT = 1 << 17, /* -c */
 #endif
-#define FILEUTILS_CP_OPTSTR "pdRfilsLHarPvuTt:" IF_SELINUX("c")
+#define FILEUTILS_CP_OPTSTR "pdRfinlsLHarPvuTt:" IF_SELINUX("c")
 /* How many bits in FILEUTILS_CP_OPTSTR? */
-	FILEUTILS_CP_OPTNUM       = 17 - !ENABLE_SELINUX,
+	FILEUTILS_CP_OPTBITS      = 18 - !ENABLE_SELINUX,
 
-	FILEUTILS_RMDEST          = 1 << (17 - !ENABLE_SELINUX), /* --remove-destination */
+	FILEUTILS_RMDEST          = 1 << (19 - !ENABLE_SELINUX), /* cp --remove-destination */
 	/* bit 18 skipped for "cp --parents" */
-	FILEUTILS_REFLINK         = 1 << (19 - !ENABLE_SELINUX), /* cp --reflink=auto */
-	FILEUTILS_REFLINK_ALWAYS  = 1 << (20 - !ENABLE_SELINUX), /* cp --reflink[=always] */
+	FILEUTILS_REFLINK         = 1 << (20 - !ENABLE_SELINUX), /* cp --reflink=auto */
+	FILEUTILS_REFLINK_ALWAYS  = 1 << (21 - !ENABLE_SELINUX), /* cp --reflink[=always] */
 	/*
 	 * Hole. cp may have some bits set here,
 	 * they should not affect remove_file()/copy_file()

libbb/copy_file.c
@@ -111,6 +111,8 @@ int FAST_FUNC copy_file(const char *source, const char *dest, int flags)
 			bb_error_msg("'%s' and '%s' are the same file", source, dest);
 			return -1;
 		}
+		if (flags & FILEUTILS_NO_OVERWRITE) /* cp -n */
+			return 0;
 		dest_exists = 1;
 	}
 

commit ec0c5cc142f1f9ea57235df5d093fbe180ad9c7d

cp: fix --sparse to have mandatory argument, fix --remove-destination bit position
cp --remove-destination was broken by commit 56bbbfae "cp: implement -n"

function                                             old     new   delta
cp_main                                              527     533      +6

Signed-off-by: Denys Vlasenko <vda.linux@googlemail.com>

coreutils/cp.c
@@ -113,6 +113,8 @@
 #include "libbb.h"
 #include "libcoreutils/coreutils.h"
 
+#define DBG_OPTION_PARSING 0
+
 /* This is a NOEXEC applet. Be very careful! */
 
 int cp_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
@@ -168,7 +170,7 @@ int cp_main(int argc, char **argv)
 		"reflink\0"        Optional_argument "\xfd"
 # endif
 # if ENABLE_FEATURE_CP_SPARSE
-		"sparse\0"         Optional_argument "\xfc"
+		"sparse\0"         Required_argument "\xfc"
 # endif
 		, &last
 # if ENABLE_FEATURE_CP_REFLINK
@@ -179,6 +181,7 @@ int cp_main(int argc, char **argv)
 		, &sparse
 # endif
 	);
+
 # if ENABLE_FEATURE_CP_REFLINK
 	BUILD_BUG_ON((int)OPT_reflink != (int)FILEUTILS_REFLINK);
 	if (flags & FILEUTILS_REFLINK) {
@@ -189,6 +192,16 @@ int cp_main(int argc, char **argv)
 		else if (strcmp(reflink, "auto") != 0)
 			bb_show_usage();
 	}
+# endif
+# if DBG_OPTION_PARSING
+	bb_error_msg("flags: 0x%08x", flags);
+#  define showopt(o) bb_error_msg("flags & %s(%x):\t%x", #o, o, flags & o);
+	showopt(FILEUTILS_RMDEST        );
+	showopt(OPT_parents             );
+	showopt(OPT_reflink             );
+	showopt(FILEUTILS_REFLINK_ALWAYS);
+	return 0;
+#  undef showopt
 # endif
 #else
 	flags = getopt32(argv, "^"

include/libbb.h
@@ -505,16 +505,17 @@ enum {	/* cp.c, mv.c, install.c depend on these values. CAREFUL when changing th
 #endif
 #define FILEUTILS_CP_OPTSTR "pdRfinlsLHarPvuTt:" IF_SELINUX("c")
 /* How many bits in FILEUTILS_CP_OPTSTR? */
-	FILEUTILS_CP_OPTBITS      = 18 - !ENABLE_SELINUX,
+	FILEUTILS_CP_OPTBITS      = (18 - !ENABLE_SELINUX),
 
-	FILEUTILS_RMDEST          = 1 << (19 - !ENABLE_SELINUX), /* cp --remove-destination */
-	/* bit 18 skipped for "cp --parents" */
+	FILEUTILS_RMDEST          = 1 << (18 - !ENABLE_SELINUX), /* cp --remove-destination */
+	/* bit 19 is used in cp for "cp --parents" */
 	FILEUTILS_REFLINK         = 1 << (20 - !ENABLE_SELINUX), /* cp --reflink=auto */
-	FILEUTILS_REFLINK_ALWAYS  = 1 << (21 - !ENABLE_SELINUX), /* cp --reflink[=always] */
+	/* bit 21 is used in cp for "cp --sparse=... */
 	/*
 	 * Hole. cp may have some bits set here,
 	 * they should not affect remove_file()/copy_file()
 	 */
+	FILEUTILS_REFLINK_ALWAYS  = 1 << 29, /* cp --reflink, --reflink=always */
 #if ENABLE_SELINUX
 	FILEUTILS_SET_SECURITY_CONTEXT = 1 << 30,
 #endif